fb0733e407afda2d560b6f24a5fc80955915e23b
Nova Amiko <beckhv2@gmail.com>
2023-04-11 20:43:32 -0800

    added first four functions to code

--- soc_sec_data.py
+++ soc_sec_data.py
@@ -11,3 +11,39 @@ Github: https://github.com/bexcoding
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def create_df(search_year):
+    # creates data frame
+    # replace file directory with correct local directory
+    return pd.read_csv(f"/home/nova/Downloads/names/yob{search_year}.txt", 
+                     names=["name", "sex", "number"])
+
+def get_gender(df, gender):
+    # return either mal or female half of df
+    if gender == "M":
+        return df[df["sex"] == "M"]
+    elif gender == "F":
+        return df[df["sex"] == "F"]
+    
+def add_number_total(df):
+    # add number column
+    return df["number"].sum()
+
+def top_ten(df, gender):
+    # create df and get top ten names for men or women
+    top_ten_df = get_gender(df, gender).iloc[:10]
+    return np.array(top_ten_df["name"])
+
+
+
+
+print(add_number_total(get_gender(create_df(1880), "F")))
+#d = create_df(1880)
+#print(np.array(d["number"]).sum())
+#print(create_df(1880)["number"].sum())
+#print(top_ten(1880, "M"))    
+#return pd.concat([women, men])
+
+#print(top_ten(2000).loc[17658])
+#print(create_df(1999).iloc[:10])
