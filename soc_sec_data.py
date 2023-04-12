43c23feb5dad730479560f4624d7754d0b79646d
Nova Amiko <beckhv2@gmail.com>
2023-04-11 18:40:17 -0800

    added file with code signature and imports

---
+++ soc_sec_data.py
@@ -0,0 +1,13 @@
+"""
+Title: Social Security Data - Baby Names
+Description: Work with 140 years of baby names in the US.
+Last Updated: April 11, 2023
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
